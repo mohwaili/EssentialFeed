c52fec8266d701621a73707392bd3ccd59567779
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-02-07 20:51:57 +0100

    add file and line to the getFeedResult function

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -31,13 +31,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers -
     
-    private func getFeedResult() -> Result<[FeedItem], Error>? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> Result<[FeedItem], Error>? {
         let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: url, client: client)
         
-        trackForMemoryLeaks(client)
-        trackForMemoryLeaks(loader)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "wait for load completion")
         var receivedResult: Result<[FeedItem], Error>?
