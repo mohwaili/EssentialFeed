f210f38e1c6cd5ccf22826695e01e52543a305d9
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-02-13 11:20:56 +0100

    move 'LocalFeedItem' to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mohammed Alwaili on 13/02/2021.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+
+}
