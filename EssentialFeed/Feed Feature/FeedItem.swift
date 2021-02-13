251fdb5c8d9f5dc4e2664b54bf635c35be7856c8
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-02-13 10:28:39 +0100

    extract FeedItem to its own file and remove the == static func implementaion

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mohammed Al Waili on 03/02/2021.
+//
+
+import Foundation
+
+public struct FeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+
+}
--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -2,31 +2,10 @@
 //  FeedLoader.swift
 //  EssentialFeed
 //
-//  Created by Mohammed Al Waili on 03/02/2021.
+//  Created by Mohammed Alwaili on 13/02/2021.
 //
 
 import Foundation
-
-public struct FeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-    
-    public static func ==(lhs: FeedItem, rhs: FeedItem) -> Bool {
-        lhs.id == rhs.id
-            && lhs.description == rhs.description
-            && lhs.location == rhs.location
-            && lhs.imageURL == rhs.imageURL
-    }
-}
 
 public protocol FeedLoader {
     func load(completion: @escaping (Result<[FeedItem], Swift.Error>) -> Void)
