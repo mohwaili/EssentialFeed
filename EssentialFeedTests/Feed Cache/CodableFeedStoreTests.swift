b2465db9e94d35efea17e3d4666ab6c9284f23a2
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-02-23 00:56:23 +0100

    add new test

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -12,6 +12,12 @@ class CodableFeedStore {
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.empty)
     }
+    
+    func insert(_ feed: [LocalFeedImage],
+                timestamp: Date,
+                completion: @escaping FeedStore.InsertionCompletion) {
+        completion(nil)
+    }
 }
 
 class CodableFeedStoreTests: XCTestCase {
@@ -27,6 +33,49 @@ class CodableFeedStoreTests: XCTestCase {
                 XCTFail("expected empty result, but got \(result) instead!")
             }
             exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+    }
+    
+    func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "wait for completion")
+        
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty): break
+                default:
+                    XCTFail("expected empty result, but got \(firstResult) and \(secondResult) instead!")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "wait for completion")
+        
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "expcted the feed to be inserted without error")
+            sut.retrieve { retrieveResult in
+                switch retrieveResult {
+                case let .found(feed: retrievedFeed, timestamp: retrievedTimestamp):
+                    XCTAssertEqual(retrievedFeed, feed)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                default:
+                    XCTFail("expected empty result, but got \(retrieveResult) instead!")
+                }
+                exp.fulfill()
+            }
         }
         
         wait(for: [exp], timeout: 1.0)
